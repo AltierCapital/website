597b9ddf99635d012e730bb0f9c4cf5046de2c3b
pnodet <paul.nodet@gmail.com>
2023-12-10 09:07:59 +0700

    fix(home): prevent scroll-y and update h1 content and colors

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import SphereImage from "~/images/sphere.svg";
 
 const Home = () => (
-  <>
+  <div className="relative w-screen h-screen overflow-hidden">
     <div className="absolute h-full w-full bottom-0 flex items-end">
       <div className="relative mx-auto max-h-64">
         <Image
@@ -14,8 +14,8 @@ const Home = () => (
         />
       </div>
     </div>
-    <section className="w-full h-full flex flex-col justify-center items-center gap-y-8 z-10">
-      <h1 className="text-transparent bg-clip-text bg-gradient-to-b from-gray-300 to-gray-700 text-4xl md:text-5xl lg:text-6xl font-clashGrotesk font-medium">
+    <div className="w-full h-full flex flex-col justify-center items-center gap-y-8 z-10">
+      <h1 className="text-transparent bg-clip-text bg-gradient-to-b from-gray-50 to-gray-500/20 text-4xl md:text-5xl lg:text-6xl font-clashGrotesk font-medium">
         Altier Capital{" "}
         <span role="image" aria-label="sparkles" className="text-white">
           ✨
@@ -23,7 +23,7 @@ const Home = () => (
       </h1>
 
       <p className="text-white max-w-xl text-center text-sm md:text-base px-10">
-        Dive into the elite world of AC, where the future of finance meets the
+        Dive into an elite world, where the future of finance meets the
         innovation of web3. Unlocking unparalleled access to groundbreaking web3
         ventures. Are you ready to be at the forefront of the financial
         revolution?
@@ -33,8 +33,8 @@ const Home = () => (
         <Button variant="secondary">Portfolio</Button>
         <GradientButton>Contact us</GradientButton>
       </div>
-    </section>
-  </>
+    </div>
+  </div>
 );
 
 export default Home;
