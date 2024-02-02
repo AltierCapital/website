593abdd9c9cd567f48c545f483cb0c2d1d35905d
Delioos <htz_j@protonmail.com>
2024-02-02 17:50:58 +0100

    fix(code): layout className blank space

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,7 @@ export const metadata: Metadata = {
 
 const RootLayout = ({ children }: { readonly children: React.ReactNode }) => (
   <html lang="fr" className={cn("min-w-[360px] scroll-smooth bg-black", fonts)}>
-    <body className="flex !h-[unset] min-h-screen flex-1 flex-col font-sans antialiased overflox-hidden ">
+    <body className="flex !h-[unset] min-h-screen flex-1 flex-col font-sans antialiased">
       <Navbar />
 
       <main className="flex w-full max-w-[100vw] flex-col">{children}</main>
