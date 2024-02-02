767b2447c11dd2deb5cedb4d8fd173ba3fbd1745
Delioos <htz_j@protonmail.com>
2024-02-02 17:18:33 +0100

    fix(contact): correct use of Script next tag

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,9 @@
+import Script from "next/script";
+
 const Contact = () => {
   return (
     <>
-      <script async src="https://tally.so/widgets/embed.js" />
+      <Script async src="https://tally.so/widgets/embed.js" />
       <iframe
         data-tally-src="https://tally.so/r/w4r77X"
         className="w-full h-screen z-10"
