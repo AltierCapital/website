a871352a9ce5f154bb9c70ef262d18795972fd35
pnodet <paul.nodet@gmail.com>
2024-02-03 09:38:17 +0800

    fix: no array index key

--- src/app/(home)/circles.tsx
+++ src/app/(home)/circles.tsx
@@ -1,19 +1,18 @@
 import { CircleBackground } from "@/components/ui/circle-background";
 
 const CIRCLES = [
-  ["w-1/3 h-1/3", "animate-spin-reverse-slower", "#3f3f46"],
-  ["w-1/2 h-1/2", "animate-spin-slow", "#71717a"],
-  ["w-4/6 h-4/6", "animate-spin-reverse-slow", "#52525b"],
-  ["w-5/6 h-5/6", "animate-spin-slower", "#3f3f46"],
+  [0, "w-1/3 h-1/3", "animate-spin-reverse-slower", "#3f3f46"],
+  [1, "w-1/2 h-1/2", "animate-spin-slow", "#71717a"],
+  [2, "w-4/6 h-4/6", "animate-spin-reverse-slow", "#52525b"],
+  [3, "w-5/6 h-5/6", "animate-spin-slower", "#3f3f46"],
 ] as const;
 
 export const HomeCirclesBackground = () => (
   <>
-    {CIRCLES.map(([className, animation, color], index) => (
+    {CIRCLES.map(([index, className, animation, color]) => (
       <div
-        // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
         key={index}
-        className={`-z-10 absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 ${className}`}
+        className={`absolute left-1/2 top-1/2 -z-10 -translate-x-1/2 -translate-y-1/2 ${className}`}
       >
         <CircleBackground
           color={color}
