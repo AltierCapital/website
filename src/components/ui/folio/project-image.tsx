071c86c81b68dd2ff4045eb482e4572c05364868
Delioos <htz_j@protonmail.com>
2024-02-07 17:36:24 +0100

    fix(folio): new glowy effect on hover

--- src/components/ui/folio/project-image.tsx
+++ src/components/ui/folio/project-image.tsx
@@ -1,5 +1,7 @@
+"use client";
 import { cn } from "@/utils/classnames";
 import Image, { StaticImageData } from "next/image";
+import React, { MouseEvent, useState } from "react";
 
 type Props = {
   readonly src: StaticImageData;
@@ -8,16 +10,43 @@ type Props = {
   readonly btnClasses?: string;
 };
 
-export const ProjectImage = ({ src, alt, href, btnClasses }: Props) => (
-  <a href={href} target="_blank" rel="noreferrer">
-    <button
-      className={cn(
-        "w-56 h-28 md:w-72 md:h-44 bg-transparent hover:bg-zinc-200/10 rounded-2xl relative p-6 transition-all duration-150 ease-in-out",
-        btnClasses,
-      )}
-      type="button"
-    >
-      <Image src={src} alt={alt} className="w-full h-full object-contain" />
-    </button>
-  </a>
-);
+export const ProjectImage = ({ src, alt, href, btnClasses }: Props) => {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseMove = (event: MouseEvent) => {
+    if (isHovered) {
+      // Update position on hover
+      setPosition({
+        x: event.nativeEvent.offsetX,
+        y: event.nativeEvent.offsetY,
+      });
+    }
+  };
+
+  const handleMouseEnter = () => setIsHovered(true);
+  const handleMouseLeave = () => setIsHovered(false);
+
+  return (
+    <a href={href} target="_blank" rel="noreferrer">
+      <button
+        onMouseMove={handleMouseMove}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        className={cn(
+          "w-56 h-28 md:w-72 md:h-44 bg-transparent  rounded-2xl relative p-6 overflow-hidden",
+          btnClasses,
+          { "group-hover:overflow-visible": isHovered }, // Glow effect on hover
+        )}
+        type="button"
+        style={{
+          backgroundImage: isHovered
+            ? `radial-gradient(circle at ${position.x}px ${position.y}px, rgba(100,   100,   100,   0.5), transparent   80%)`
+            : undefined,
+        }}
+      >
+        <Image src={src} alt={alt} className="w-full h-full object-contain" />
+      </button>
+    </a>
+  );
+};
